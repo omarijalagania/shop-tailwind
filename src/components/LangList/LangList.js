ee4a18e599ccfd0eaecdd65a8be0a3130bd1ee29
omari <omari.jalagania@gmail.com>
2022-04-09 17:45:33 +0400

    popover

--- src/components/LangList/LangList.js
+++ src/components/LangList/LangList.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Transition, Menu } from '@headlessui/react'
 import { useDispatch } from 'react-redux'
 import { changeLanguage } from '../store/Language/action'
 import Icon from '../Icon'
@@ -11,13 +12,18 @@ const LangList = ({ setActiveLang }) => {
   }
 
   return (
-    <div className='flex flex-col border-2 items-center rounded relative bg-white p-4 right-12 top-14'>
-      <Icon
-        className='mb-3'
-        filename='ge'
-        onClick={() => languageHandle('ge')}
-      />
-      <Icon filename='en' onClick={() => languageHandle('en')} />
+    <div className='w-56 text-right fixed top-16'>
+      <div className='relative inline-block'>
+        <div className='absolute right-48 -top-10 w-10 py-4 px-2 flex flex-col items-center mt-2  bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+          <Icon
+            className='mb-3'
+            filename='ge'
+            onClick={() => languageHandle('ge')}
+          />
+
+          <Icon filename='en' onClick={() => languageHandle('en')} />
+        </div>
+      </div>
     </div>
   )
 }
