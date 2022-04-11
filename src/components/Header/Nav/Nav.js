4f10bfa5386a87b4aa2acac5368102091f2b3da6
omari <omari.jalagania@gmail.com>
2022-04-11 17:45:11 +0400

    mobile menu

--- src/components/Header/Nav/Nav.js
+++ src/components/Header/Nav/Nav.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useState } from 'react'
 import { Popover, Transition, Menu } from '@headlessui/react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import { Dialog, Tab } from '@headlessui/react'
 import { XIcon } from '@heroicons/react/outline'
 import { MenuIcon, SearchIcon, ShoppingBagIcon } from '@heroicons/react/outline'
@@ -67,121 +67,47 @@ function Nav() {
               </div>
 
               {/* Links */}
-              <Tab.Group as='div' className='mt-2'>
-                <div className='border-b border-gray-200'>
-                  <Tab.List className='-mb-px flex px-4 space-x-8'>
-                    {navigation.categories.map((category) => (
-                      <Tab
-                        key={category.name}
-                        className={({ selected }) =>
-                          classNames(
-                            selected
-                              ? 'text-indigo-600 border-indigo-600'
-                              : 'text-gray-900 border-transparent',
-                            'flex-1 whitespace-nowrap py-4 px-1 border-b-2 text-base font-medium',
-                          )
-                        }
-                      >
-                        {category.name}
-                      </Tab>
-                    ))}
-                  </Tab.List>
-                </div>
-                <Tab.Panels as={Fragment}>
-                  {navigation.categories.map((category) => (
-                    <Tab.Panel
-                      key={category.name}
-                      className='pt-10 pb-8 px-4 space-y-10'
-                    >
-                      <div className='grid grid-cols-2 gap-x-4'>
-                        {category.featured.map((item) => (
-                          <div
-                            key={item.name}
-                            className='group relative text-sm'
-                          >
-                            <div className='aspect-w-1 aspect-h-1 rounded-lg bg-gray-100 overflow-hidden group-hover:opacity-75'>
-                              <img
-                                src={item.imageSrc}
-                                alt={item.imageAlt}
-                                className='object-center object-cover'
-                              />
-                            </div>
-                            <a
-                              href={item.href}
-                              className='mt-6 block font-medium text-gray-900'
-                            >
-                              <span
-                                className='absolute z-10 inset-0'
-                                aria-hidden='true'
-                              />
-                              {item.name}
-                            </a>
-                            <p aria-hidden='true' className='mt-1'>
-                              ნახვა
-                            </p>
-                          </div>
-                        ))}
-                      </div>
-                      {category.sections.map((section) => (
-                        <div key={section.name}>
-                          <p
-                            id={`${category.id}-${section.id}-heading-mobile`}
-                            className='font-medium text-gray-900'
-                          >
-                            {section.name}
-                          </p>
-                          <ul
-                            role='list'
-                            aria-labelledby={`${category.id}-${section.id}-heading-mobile`}
-                            className='mt-6 flex flex-col space-y-6'
-                          >
-                            {section.items.map((item) => (
-                              <li key={item.name} className='flow-root'>
-                                <a
-                                  href={item.href}
-                                  className='-m-2 p-2 block text-gray-500'
-                                >
-                                  {item.name}
-                                </a>
-                              </li>
-                            ))}
-                          </ul>
-                        </div>
-                      ))}
-                    </Tab.Panel>
-                  ))}
-                </Tab.Panels>
-              </Tab.Group>
-
-              <div className='border-t border-gray-200 py-6 px-4 space-y-6'>
-                {navigation.pages.map((page) => (
-                  <div key={page.name} className='flow-root'>
-                    <a
-                      href={page.href}
-                      className='-m-2 p-2 block font-medium text-gray-900'
-                    >
-                      {page.name}
-                    </a>
-                  </div>
-                ))}
-              </div>
 
               <div className='border-t border-gray-200 py-6 px-4 space-y-6'>
                 <div className='flow-root'>
+                  <Menu as='div' className='flex mb-8'>
+                    <Menu.Button className='text-gray-700 hover:text-gray-800 flex items-center'>
+                      <img
+                        src={`/images/${activeLang}.png`}
+                        alt=''
+                        className='w-7 h-auto block flex-shrink-0'
+                      />
+                      <span className='sr-only'>, change currency</span>
+                    </Menu.Button>
+
+                    <Transition
+                      as={Fragment}
+                      enter='transition ease-out duration-100'
+                      enterFrom='transform opacity-0 scale-95'
+                      enterTo='transform opacity-100 scale-100'
+                      leave='transition ease-in duration-55'
+                      leaveFrom='transform opacity-100 scale-100'
+                      leaveTo='transform opacity-0 scale-95'
+                    >
+                      <Menu.Items>
+                        <LangList setActiveLang={setActiveLang} />
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
                   <a
                     href='#'
                     className='-m-2 p-2 block font-medium text-gray-900'
                   >
-                    შესვლა
+                    {t('login')}
                   </a>
                 </div>
                 <div className='flow-root'>
-                  <a
-                    href='#'
+                  <NavLink
+                    to='/register'
                     className='-m-2 p-2 block font-medium text-gray-900'
                   >
-                    რეგისტრაცია
-                  </a>
+                    {t('register')}
+                  </NavLink>
                 </div>
               </div>
             </div>
